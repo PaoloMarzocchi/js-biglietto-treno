ca92f7a6f40badfe394e19d4fe6e9139a8a65406
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-21 18:53:18 +0100

    fix error calculating senior ticket and output message

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ const distance = Number(prompt("Inserire i Kilometri da percorrere"));
 // Calcolo il prezzo del biglietto salvandolo in una variabile
 
 const price_per_km = 0.21;
-const ticketPrice = distance * price_per_km;
+let ticketPrice = distance * price_per_km;
 //console.log(typeof ticketPrice);
 
 // Applico eventuali sconti in base all'età inserita
@@ -19,35 +19,17 @@ const ticketPrice = distance * price_per_km;
 -SE età < 18 sconto 20%
 -ALTRIMENTI SE età > 60 sconto 40%
 */
-
-let discountedPrice;
+let messageOutput = 'Biglietto Standard: ';
 
 if (userAge < 18) {
     const young_discount = ticketPrice * 0.2;
-    discountedPrice = ticketPrice - young_discount;
-    console.log(`
-    Prezzo del biglietto standard :  ${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(
-        ticketPrice,
-    )} 
-    Prezzo scontato per i minori di 18 anni : ${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(
-        discountedPrice,
-    )}
-        `);
+    ticketPrice = ticketPrice - young_discount;
+    messageOutput = 'Biglietto Junior: ';
 } else if (userAge >= 65) {
-    const old_discount = ticketPrice * 0.6;
-    discountedPrice = ticketPrice - old_discount;
-    console.log(`
-    Prezzo del biglietto standard :  ${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(
-        ticketPrice,
-    )} 
-    Prezzo scontato per gli over 65 : ${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(
-        discountedPrice,
-    )}
-        `);
-} else {
-    console.log(`
-    Prezzo del biglietto : ${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(
-        ticketPrice,
-    )}
-        `);
+    const old_discount = ticketPrice * 0.4;
+    ticketPrice = ticketPrice - old_discount;
+    messageOutput = 'Biglietto Senior: ';
 }
+
+alert(messageOutput + ticketPrice.toFixed(2) + ' €')
+
